f03df38c8bdcc23875b99f8380a4686cc9b94263
Adam Pagels <pagels@hotmail.ca>
2024-01-04 15:31:53 +0000

    First commit

---
+++ noooted/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  noooted
+//
+//  Created by Adam Pagels on 2024-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ noooted/noootedApp.swift
@@ -0,0 +1,17 @@
+//
+//  noootedApp.swift
+//  noooted
+//
+//  Created by Adam Pagels on 2024-01-03.
+//
+
+import SwiftUI
+
+@main
+struct noootedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
