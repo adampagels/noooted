248648809aa2e4fd2d7837f0f98f12e7c46012fc
Adam Pagels <pagels@hotmail.ca>
2024-01-12 12:34:30 +0000

    Update UI to neubrutalism

--- noooted/ContentView.swift
+++ noooted/ContentView.swift
@@ -13,15 +13,30 @@ struct ContentView: View {
         SortDescriptor(\.lastUpdated, order: .reverse)
     ]) var notes: FetchedResults<Note>
     
+    let colorDict: [String: Color] =
+    ["red" : .red,
+     "orange" : .orange,
+     "yellow" : .yellow,
+     "green" : .green,
+     "blue" : .blue,
+     "indigo" : .indigo,
+     "purple" : .purple
+    ]
+    
     @State private var selectedNote: Note?
     @State private var showAddScreen = false
     
     var body: some View {
         ZStack {
             List(notes) { note in
-                Button(note.title ?? "Unknown") {
-                    selectedNote = note
-                }
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8).offset(x: 3, y: 5)
+                    RoundedRectangle(cornerRadius: 8) .foregroundColor(colorDict[note.tagColor ?? "red"])
+                    Button(note.title ?? "Untitled") {
+                        selectedNote = note
+                    }.padding(10).foregroundColor(.black)
+                    RoundedRectangle(cornerRadius: 8).stroke(style:StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+                }.padding(EdgeInsets(top:5, leading: 0, bottom: 5, trailing: 0)).listRowSeparator(.hidden)
             }
             .scrollContentBackground(.hidden)
             .sheet(item: $selectedNote) { item in
